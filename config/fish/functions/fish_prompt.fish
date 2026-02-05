eedc55fe440c5c8c5f9cbb1f6e5884958128430e
Eli Sher <elijsher@gmail.com>
2026-02-05 10:43:29 -0700

    fish_prompt: configure git prompt once per session

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,34 +1,21 @@
 function fish_prompt --description 'Write out the prompt'
         set -l last_pipestatus $pipestatus
-        set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+        set -l __fish_last_status $status
     
-        if not set -q __fish_git_prompt_show_informative_status
-                set -g __fish_git_prompt_show_informative_status 1
+        if not set -q __prompt_git_cfg_done
+                set -g __prompt_git_cfg_done 1
+        
+                set -g __fish_git_prompt_showdirtystate 1
+                set -g __fish_git_prompt_showuntrackedfiles 1
+                set -g __fish_git_prompt_showupstream auto
+                set -g __fish_git_prompt_color_branch yellow
+                set -g __fish_git_prompt_char_dirtystate '✚'
+                set -g __fish_git_prompt_char_stagedstate '●'
+                set -g __fish_git_prompt_char_untrackedfiles '…'
+                set -g __fish_git_prompt_char_upstream_ahead '↑'
+                set -g __fish_git_prompt_char_upstream_behind '↓'
         end
-        if not set -q __fish_git_prompt_hide_untrackedfiles
-                set -g __fish_git_prompt_hide_untrackedfiles 1
-        end
-        if not set -q __fish_git_prompt_color_branch
-                set -g __fish_git_prompt_color_branch magenta --bold
-        end
-        if not set -q __fish_git_prompt_showupstream
-                set -g __fish_git_prompt_showupstream informative
-        end
-        if not set -q __fish_git_prompt_color_dirtystate
-                set -g __fish_git_prompt_color_dirtystate blue
-        end
-        if not set -q __fish_git_prompt_color_stagedstate
-                set -g __fish_git_prompt_color_stagedstate yellow
-        end
-        if not set -q __fish_git_prompt_color_invalidstate
-                set -g __fish_git_prompt_color_invalidstate red
-        end
-        if not set -q __fish_git_prompt_color_untrackedfiles
-                set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-        end
-        if not set -q __fish_git_prompt_color_cleanstate
-                set -g __fish_git_prompt_color_cleanstate green --bold
-        end
+        
     
         set -l color_cwd
         set -l suffix
