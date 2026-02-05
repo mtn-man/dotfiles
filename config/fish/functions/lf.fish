3333c8e209bba337622c6fd28a533b76c2d4aa34
Eli Sher <elijsher@gmail.com>
2026-02-05 12:14:22 -0700

    lf.fish: add better quoting for $tmp

--- config/fish/functions/lf.fish
+++ config/fish/functions/lf.fish
@@ -1,12 +1,14 @@
 function lf --description 'lf with quit-and-cd integration'
     set -l tmp (mktemp)
-    # Using 'command lf' ensures we call the binary, not the function recursively
-    command lf -last-dir-path=$tmp $argv
-    if test -f $tmp
-        set -l dir (cat $tmp)
-        rm -f $tmp
-        if test -d "$dir" -a "$dir" != (pwd)
-            cd "$dir"
+
+    command lf -last-dir-path="$tmp" $argv
+
+    if test -f "$tmp"
+        set -l dir (cat "$tmp")
+        rm -f "$tmp"
+
+        if test -n "$dir"; and test -d "$dir"; and test "$dir" != (pwd)
+            cd -- "$dir"
         end
     end
 end
