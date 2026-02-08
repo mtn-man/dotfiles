6852e2f2e5c07f18fc5abaed74eaa827f29a219a
Eli Sher <elijsher@gmail.com>
2026-02-07 17:19:27 -0700

    cdl: correctd behavior for case where argument is left blank - now returns to ~

--- config/fish/functions/cdl.fish
+++ config/fish/functions/cdl.fish
@@ -1,11 +1,9 @@
 function cdl --wraps=cd --description 'Change directory and list contents with eza'
     if test (count $argv) -gt 0
-        # Change directory to the first argument
         cd "$argv[1]"
-        # Run eza with the remaining arguments ($argv[2..-1])
-        and eza -lh --git --group-directories-first --header $argv[2..-1]
+        and eza --git --group-directories-first --header $argv[2..-1]
     else
-        # Default behavior: list current directory
-        eza -lh --git --group-directories-first --header
+        cd
+        and eza --git --group-directories-first --header
     end
 end
