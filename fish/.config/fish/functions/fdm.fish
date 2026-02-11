7a85d27c4fd7d05c3e01ca1f640748a560c51e0e
Eli Sher <elijsher@gmail.com>
2026-02-11 14:13:44 -0700

    fdm.fish: add fuzzy finding and dotfile support

--- fish/.config/fish/functions/fdm.fish
+++ fish/.config/fish/functions/fdm.fish
@@ -1,14 +1,14 @@
 function fdm --description 'Open file in micro via fd search'
    if test (count $argv) -eq 0
-        echo "Usage: mf <pattern>"
+        echo "Usage: fdm <pattern>"
         return 1
     end
 
     # Use glob matching so you don’t need to escape dots
-    set -l matches (fd -L -t f -g "$argv[1]" ~/dev)
+    set -l matches (fd -L -H -t f -g "*$argv[1]*" ~/dev)
 
     if test (count $matches) -eq 0
-        echo "mf: no matches found"
+        echo "fdm: no matches found"
         return 1
     end
 
