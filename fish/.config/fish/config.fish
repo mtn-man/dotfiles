b4e619a34e542589ee300ff01d67b89fb08b0955
Eli Sher <elijsher@gmail.com>
2026-02-12 13:17:29 -0700

    Add aliases.fish, source it from config.fish, and remove large alias block from the config. Add alias for tping instead of discrete function file. Remove functions/tping.fish as it is now redundant/

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,32 +22,7 @@ fish_add_path -gP ~/go/bin
 
 # 4. Interactive Session Configuration
 if status is-interactive
-    # Using alias instead of abbr for internal function compatibility
-    alias u='update'
-    alias t='timer'
-    alias t3='timer 3m'
-    alias t4='timer 4m'
-    alias t5='timer 5m'
-    alias t10='timer 10m'
-    alias t20='timer 20m'
-    alias t30='timer 30m'
-    alias mm='mintmedia'
-    alias m='micro'
-    alias mn='media-on'
-    alias mf='media-off'
-    alias nu='nord-up'
-    alias nd='nord-down'
-    alias l='eza'
-    alias c='bat'
-    alias ff='fastfetch'
-    alias yto='yt -o'
-    alias yti='yt -i'
-    alias tm-on='brew services start transmission-cli'
-    alias tm-off='brew services stop transmission-cli'
-    alias tm-re='brew services restart transmission-cli'
-    alias z='cd'
-    alias speed='networkQuality'
-    alias lg='lazygit'
+    source ~/.config/fish/aliases.fish
 end
 
 # 5. Zoxide alias
--- fish/.config/fish/functions/tping.fish
+++
@@ -1,3 +0,0 @@
-function tping --description 'Quick Transmission activity check'
-    transmission-remote 127.0.0.1:9091 -st
-end
