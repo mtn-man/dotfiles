0711bf75e7e1f1d3e72c0f2f1c83d5f5cf0e90f1
Eli Sher <elijsher@gmail.com>
2026-02-12 15:34:50 -0700

    cdl.fish: patch to no longer fail silently when passed non-existent dirs

--- fish/.config/fish/functions/cdl.fish
+++ fish/.config/fish/functions/cdl.fish
@@ -1,9 +1,9 @@
 function cdl --wraps=cd --description 'Change directory and list contents with eza'
     if test (count $argv) -gt 0
-        cd "$argv[1]"
-        and eza --git --group-directories-first --header $argv[2..-1]
+        cd "$argv[1]"; or return 1
+        eza --git --group-directories-first --header $argv[2..-1]
     else
-        cd
-        and eza --git --group-directories-first --header
+        cd; or return 1
+        eza --git --group-directories-first --header
     end
 end
