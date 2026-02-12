1ccb2dd2eb44372bb0319ee800df9ef6e33dbe38
Eli Sher <elijsher@gmail.com>
2026-02-11 19:07:50 -0700

    add fm function (rename from fdm)

---
+++ fish/.config/fish/functions/fm.fish
@@ -0,0 +1,31 @@
+function fm --description 'Open file in micro via fd search (fzf when multiple matches)'
+    if test (count $argv) -eq 0
+        echo "Usage: fm <pattern>"
+        return 1
+    end
+
+    # Find matches under ~/dev (follow symlinks, include hidden)
+    set -l matches (fd -L -H -t f -g "*$argv[1]*" ~/dev)
+
+    switch (count $matches)
+        case 0
+            echo "fm: no matches found"
+            return 1
+
+        case 1
+            micro $matches[1]
+            return 0
+
+        case '*'
+            # Pick one interactively
+            set -l chosen (printf '%s\n' $matches | fzf -i --prompt='fm> ')
+
+            if test (count $chosen) -eq 0
+                echo "fm: cancelled"
+                return 1
+            end
+
+            # fzf returns newline-separated paths; fish splits them into a list automatically
+            micro $chosen
+    end
+end
