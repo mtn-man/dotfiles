4c794d3a4d17f47f36f4ea5bd814ce07a9c0fa04
Eli Sher <elijsher@gmail.com>
2026-02-11 18:27:23 -0700

    fdm.fish: integrate fzf for refined selection only when matching multiple files instead of opening them all

--- fish/.config/fish/functions/fdm.fish
+++ fish/.config/fish/functions/fdm.fish
@@ -1,17 +1,31 @@
-function fdm --description 'Open file in micro via fd search'
-   if test (count $argv) -eq 0
+function fdm --description 'Open file in micro via fd search (fzf when multiple matches)'
+    if test (count $argv) -eq 0
         echo "Usage: fdm <pattern>"
         return 1
     end
 
-    # Use glob matching so you don’t need to escape dots
+    # Find matches under ~/dev (follow symlinks, include hidden)
     set -l matches (fd -L -H -t f -g "*$argv[1]*" ~/dev)
 
-    if test (count $matches) -eq 0
-        echo "fdm: no matches found"
-        return 1
-    end
+    switch (count $matches)
+        case 0
+            echo "fdm: no matches found"
+            return 1
 
-    # If one or more matches exist, open them in micro
-    micro $matches
+        case 1
+            micro $matches[1]
+            return 0
+
+        case '*'
+            # Pick one (or many) interactively
+            set -l chosen (printf '%s\n' $matches | fzf -i --prompt='fdm> ')
+
+            if test (count $chosen) -eq 0
+                echo "fdm: cancelled"
+                return 1
+            end
+
+            # fzf returns newline-separated paths; fish splits them into a list automatically
+            micro $chosen
+    end
 end
