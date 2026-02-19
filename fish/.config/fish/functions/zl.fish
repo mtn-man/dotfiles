c19d688e60dfa0fd668ff10172161952ab1babb8
Eli Sher <elijsher@gmail.com>
2026-02-19 04:01:35 -0700

    match eza flags in aliases.fish and zl.fish for consistency

--- fish/.config/fish/functions/zl.fish
+++ fish/.config/fish/functions/zl.fish
@@ -1,9 +1,9 @@
 function zl --wraps=cd --description 'Change directory (zoxide) and list contents with eza'
     if test (count $argv) -gt 0
         z "$argv[1]"; or return 1
-        eza --git --group-directories-first --header $argv[2..-1]
+        eza --git --group-directories-first $argv[2..-1]
     else
         z; or return 1
-        eza --git --group-directories-first --header
+        eza --git --group-directories-first
     end
 end
