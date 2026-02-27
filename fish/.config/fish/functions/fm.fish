dbde316a311710d8db8784cf72758d61f24fec4b
Eli Sher <elijsher@gmail.com>
2026-02-27 07:15:22 -0700

    fm.fish: add --exclude .git and use local var for fd options (DRY)

--- fish/.config/fish/functions/fm.fish
+++ fish/.config/fish/functions/fm.fish
@@ -4,14 +4,17 @@ function fm --description 'Open file in micro via fd search (fzf when multiple m
         return 1
     end
 
-    # Find matches under ~/dev (follow symlinks, include hidden)
-    set -l matches (fd --no-ignore -L -H -t f -g "*$argv[1]*" ~/dev)
-    
-    # Fallback: if nothing found, search current working directory
+    # Common fd options
+    set -l fd_opts --no-ignore -L -H -t f --exclude .git
+
+    # Search under ~/dev
+    set -l matches (fd $fd_opts -g "*$argv[1]*" ~/dev)
+
+    # Fallback: search current working directory if no matches
     if test (count $matches) -eq 0
-        set matches (fd --no-ignore -L -H -t f -g "*$argv[1]*" .)
+        set matches (fd $fd_opts -g "*$argv[1]*" .)
     end
-    
+
     switch (count $matches)
         case 0
             echo "fm: no matches found"
@@ -22,11 +25,10 @@ function fm --description 'Open file in micro via fd search (fzf when multiple m
             return 0
 
         case '*'
-            # Pick one interactively with preview
             set -l chosen (printf '%s\n' $matches | fzf -i \
-                 --prompt='fm> ' \
-                 --preview='bat --color=always --style=plain --theme="ansi" {}' \
-                 --preview-window='right:60%:wrap')
+                --prompt='fm> ' \
+                --preview='bat --color=always --style=plain --theme="ansi" {}' \
+                --preview-window='right:60%:wrap')
 
             if test (count $chosen) -eq 0
                 echo "fm: cancelled"
