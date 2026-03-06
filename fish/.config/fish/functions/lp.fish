7b01709a8f55f03acc3c7b2eab0730dc5c494a47
Eli Sher <elijsher@gmail.com>
2026-03-06 07:26:04 -0700

    fish_prompt: switch to > for prompt symbol; lp.fish: show missing paths in red

--- fish/.config/fish/functions/lp.fish
+++ fish/.config/fish/functions/lp.fish
@@ -1,5 +1,12 @@
-function lp --description 'Show PATH entries in order'
+function lp --description 'Show PATH entries in order with existence check'
     for i in (seq (count $PATH))
-        printf "%5d  %s\n" $i $PATH[$i]
+        set -l dir $PATH[$i]
+        if test -d "$dir"
+            printf "%5d  %s\n" $i $dir
+        else
+            set_color red
+            printf "%5d  %s (missing)\n" $i $dir
+            set_color normal
+        end
     end
 end
