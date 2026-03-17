eb6b7fe03b26469fd5e4948ab1be34428c3b765d
Eli Sher <elijsher@gmail.com>
2026-03-17 15:49:40 -0600

    fish_prompt.fish: fix - ensure consistent 4 space indentation

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,48 +1,47 @@
 function fish_prompt --description 'Write out the prompt'
-        set -l last_pipestatus $pipestatus
-        set -l __fish_last_status $status
-    
-        if not set -q __prompt_git_cfg_done
-                set -g __prompt_git_cfg_done 1
-        
-                set -g __fish_git_prompt_showdirtystate 1
-                set -g __fish_git_prompt_showuntrackedfiles 1
-                set -g __fish_git_prompt_showupstream auto
-                set -g __fish_git_prompt_color_branch yellow
-                set -g __fish_git_prompt_char_dirtystate '✚'
-                set -g __fish_git_prompt_char_stagedstate '●'
-                set -g __fish_git_prompt_char_untrackedfiles '…'
-                set -g __fish_git_prompt_char_upstream_ahead '↑'
-                set -g __fish_git_prompt_char_upstream_behind '↓'
-        end
-        
-    
-        set -l color_cwd
-        set -l suffix
-        if functions -q fish_is_root_user; and fish_is_root_user
-                if set -q fish_color_cwd_root
-                        set color_cwd $fish_color_cwd_root
-                else
-                        set color_cwd $fish_color_cwd
-                end
-                set suffix '#'
+    set -l last_pipestatus $pipestatus
+    set -l __fish_last_status $status
+
+    if not set -q __prompt_git_cfg_done
+        set -g __prompt_git_cfg_done 1
+
+        set -g __fish_git_prompt_showdirtystate 1
+        set -g __fish_git_prompt_showuntrackedfiles 1
+        set -g __fish_git_prompt_showupstream auto
+        set -g __fish_git_prompt_color_branch yellow
+        set -g __fish_git_prompt_char_dirtystate '✚'
+        set -g __fish_git_prompt_char_stagedstate '●'
+        set -g __fish_git_prompt_char_untrackedfiles '…'
+        set -g __fish_git_prompt_char_upstream_ahead '↑'
+        set -g __fish_git_prompt_char_upstream_behind '↓'
+    end
+
+    set -l color_cwd
+    set -l suffix
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
         else
-                set color_cwd $fish_color_cwd
-                set suffix '➤'
+            set color_cwd $fish_color_cwd
         end
-    
-        # PWD
-        set_color $color_cwd
-        echo -n (prompt_pwd)
-        set_color normal
-    
-        printf '%s ' (fish_vcs_prompt)
-    
-        set -l status_color (set_color $fish_color_status)
-        set -l statusb_color (set_color --bold $fish_color_status)
-        set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-        echo -n $prompt_status
-        set_color normal
-    
-        echo -n "$suffix "
+        set suffix '#'
+    else
+        set color_cwd $fish_color_cwd
+        set suffix '➤'
+    end
+
+    # PWD
+    set_color $color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+
+    printf '%s ' (fish_vcs_prompt)
+
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    echo -n $prompt_status
+    set_color normal
+
+    echo -n "$suffix "
 end
