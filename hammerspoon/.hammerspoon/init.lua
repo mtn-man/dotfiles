fdca1ddf1451b708154b53ef94389b7275c555ca
Eli Sher <elijsher@gmail.com>
2026-03-30 12:04:16 -0600

    Brewfile: add hammerspoon and assoc config

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,13 @@
+local function usbDeviceCallback(data)
+    if data["productName"] == "USB Receiver" and data["vendorID"] == 1133 then
+        if data["eventType"] == "added" then
+            hs.application.launchOrFocus("LinearMouse")
+        elseif data["eventType"] == "removed" then
+            local app = hs.application.find("LinearMouse")
+            if app then app:kill() end
+        end
+    end
+end
+
+local usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
+usbWatcher:start()
