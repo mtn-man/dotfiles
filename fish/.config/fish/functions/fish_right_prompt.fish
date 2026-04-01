e225490745194ac447ed6fde235c044e7c2f0f80
Eli Sher <elijsher@gmail.com>
2026-04-01 08:02:16 -0600

    fish: add functions/fish_prompt_right.fish

---
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,8 @@
+function fish_right_prompt
+  if test $CMD_DURATION -gt 3000
+      set -l secs (math "$CMD_DURATION / 1000")
+      set_color brblack
+      echo -n {$secs}s
+      set_color normal
+  end
+end
