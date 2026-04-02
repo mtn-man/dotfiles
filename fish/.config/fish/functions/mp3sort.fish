3f778b10d74a3e6d15b1014fbd7f6a7c017a72e5
Eli Sher <elijsher@gmail.com>
2026-04-02 04:49:44 -0600

    fish: add function mp3sort.fish

---
+++ fish/.config/fish/functions/mp3sort.fish
@@ -0,0 +1,28 @@
+function mp3sort --description "Organize MP3s from 'Artist - Album - Track.mp3' into Artist/Album/Track.mp3"
+    set -l dir ~/Downloads
+
+    set -l files $dir/*.mp3
+    if test (count $files) -eq 0
+        echo "No .mp3 files found in $dir"
+        return 1
+    end
+
+    for f in $files
+        set -l base (basename $f .mp3)
+        set -l parts (string split " - " $base)
+
+        if test (count $parts) -ne 3
+            echo "Skipping (expected 3 parts): $base.mp3"
+            continue
+        end
+
+        set -l artist $parts[1]
+        set -l album $parts[2]
+        set -l track $parts[3]
+        set -l dest $dir/$artist/$album
+
+        mkdir -p $dest
+        mv $f $dest/$track.mp3
+        echo "Moved: $base.mp3 → $artist/$album/$track.mp3"
+    end
+end
