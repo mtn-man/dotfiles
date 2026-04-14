45194f94e01c130fae30252b639fe3a5c84baf53
Eli Sher <elijsher@gmail.com>
2026-04-13 21:03:23 -0600

    fish_right_prompt: use idiomatic Fish string interpolation ("$secs"s)

--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -2,7 +2,7 @@ function fish_right_prompt
   if test $CMD_DURATION -gt 3000
       set -l secs (math "$CMD_DURATION / 1000")
       set_color brblack
-      echo -n {$secs}s
+      echo -n "$secs"s
       set_color normal
   end
 end
