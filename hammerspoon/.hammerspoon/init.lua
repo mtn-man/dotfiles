a077b51d7daf41b87180ca382fc43255a487ada8
Eli Sher <elijsher@gmail.com>
2026-04-16 15:19:44 -0600

    hammerspoon: run tailscale up on home network connect

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -11,3 +11,10 @@ end
 
 local usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
 usbWatcher:start()
+
+local wifiWatcher = hs.wifi.watcher.new(function()
+    if hs.wifi.currentNetwork() == "AOL Dialup" then
+        hs.execute("/opt/homebrew/bin/tailscale up", true)
+    end
+end)
+wifiWatcher:start()
