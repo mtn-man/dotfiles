149391b1f525587af18911cda74432e623125b2e
Eli Sher <elijsher@gmail.com>
2026-04-22 10:05:08 -0600

    fish_right_prompt: guard against unset CMD_DURATION

--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,8 +1,9 @@
 function fish_right_prompt
-  if test $CMD_DURATION -gt 3000
-      set -l secs (math "$CMD_DURATION / 1000")
-      set_color brblack
-      echo -n "$secs"s
-      set_color normal
+  if not set -q CMD_DURATION; or test $CMD_DURATION -le 3000
+      return
   end
+  set -l secs (math "$CMD_DURATION / 1000")
+  set_color brblack
+  echo -n "$secs"s
+  set_color normal
 end
