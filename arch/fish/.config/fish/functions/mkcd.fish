dfe5b445b054dc80156c0fd0f19690b54bfd562e
Eli Sher <elijsher@gmail.com>
2026-04-29 17:16:53 -0600

    arch: remove arch config

--- arch/fish/.config/fish/functions/mkcd.fish
+++
@@ -1,9 +0,0 @@
-function mkcd --description 'Create a directory and cd into it'
-    if test (count $argv) -lt 1
-        echo "mkcd: usage - mkcd <directory> (e.g. mkcd src)" >&2
-        return 1
-    end
-
-    mkdir -p "$argv[1]"
-    and cd "$argv[1]"
-end
