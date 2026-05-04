4df9a461918b448b5426c614bddb30c925ca434f
Eli Sher <elijsher@gmail.com>
2026-05-04 10:44:37 -0600

    vpn: update completions for mode-split subcommands

--- fish/.config/fish/completions/vpn.fish
+++ fish/.config/fish/completions/vpn.fish
@@ -1,4 +1,5 @@
 complete -c vpn -f
-complete -c vpn -n __fish_use_subcommand -a on -d 'Connect to VPN'
-complete -c vpn -n __fish_use_subcommand -a off -d 'Disconnect from VPN'
-complete -c vpn -n __fish_use_subcommand -a status -d 'Show VPN connection status'
+complete -c vpn -n __fish_use_subcommand -a on     -d 'Enforce normal mode (VPN up, Tailscale down)'
+complete -c vpn -n __fish_use_subcommand -a off    -d 'Enforce media mode (Tailscale up, VPN down)'
+complete -c vpn -n __fish_use_subcommand -a status -d 'Show current network mode'
+complete -c vpn -n __fish_use_subcommand -a toggle -d 'Switch between normal and media mode'
