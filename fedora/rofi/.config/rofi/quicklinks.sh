0f23ee41feb89135f0c0a1cbd9c7a3f6e82043a0
mtn-man <elisher@duck.com>
2026-05-20 09:19:28 -0600

    rofi: remove comment, use browser icon for links, remove whitespace

--- fedora/rofi/.config/rofi/quicklinks.sh
+++ fedora/rofi/.config/rofi/quicklinks.sh
@@ -4,5 +4,10 @@ if [[ "$ROFI_RETV" -eq 1 ]]; then
     url=$(awk -v k="$1" '$1==k{print $2}' "$links")
     [[ -n "$url" ]] && (sleep 0.1 && xdg-open "$url") >/dev/null 2>&1 &
 else
-    awk '{print $1}' "$links"
+    icon=""
+    [[ -f /usr/share/icons/hicolor/48x48/apps/brave-origin-beta.png ]] && icon="brave-origin-beta"
+    awk -v icon="$icon" '{
+        if (icon != "") printf "%s\0icon\x1f%s\n", $1, icon
+        else print $1
+    }' "$links"
 fi
