ec78605ba1c537b0f610223468a55cabc34f9de5
mtn-man <elisher@duck.com>
2026-05-21 09:51:04 -0600

    fedora: add flatpak update and unused runtime cleanup to update function

--- fedora/fish/.config/fish/functions/update.fish
+++ fedora/fish/.config/fish/functions/update.fish
@@ -1,5 +1,7 @@
-function update --description 'Update system packages via dnf'
+function update --description 'Update system packages via dnf and flatpak'
     sudo dnf upgrade --refresh; or return 1
+    flatpak update -y; or return 1
+    flatpak uninstall --unused -y
 
     echo (set_color normal --bold)"update complete"(set_color normal)
 end
