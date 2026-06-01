94a280e2f5bd5e193b45f4e53b12a83d3e5e8001
mtn-man <elisher@duck.com>
2026-06-01 07:37:16 -0600

    rofi: split actions into persist and silent types

--- fedora/rofi/.config/rofi/actions.sh
+++ fedora/rofi/.config/rofi/actions.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-functions_file="$HOME/.config/rofi/actions"
+actions_file="$HOME/.config/rofi/actions"
 
 if [[ "$ROFI_RETV" -eq 1 ]]; then
-    systemd-run --user --no-block kitty fish -c "$1; exec fish" >/dev/null 2>&1
+    if [[ "$ROFI_INFO" == "silent" ]]; then
+        systemd-run --user --no-block fish -c "$1; dunstify '$1'" >/dev/null 2>&1
+    else
+        systemd-run --user --no-block kitty fish -c "$1; exec fish" >/dev/null 2>&1
+    fi
 else
     icon=""
     kitty_icon=(/usr/share/icons/hicolor/*/apps/kitty.png)
     [[ -f "${kitty_icon[0]}" ]] && icon=$(basename "${kitty_icon[0]}" .png)
-    while IFS= read -r name; do
-        if [[ -n "$icon" ]]; then
-            printf "%s\0icon\x1f%s\n" "$name" "$icon"
+    while IFS= read -r line; do
+        if [[ "$line" == *"~" ]]; then
+            name="${line%\~}"
+            info="silent"
         else
-            echo "$name"
+            name="$line"
+            info="persist"
         fi
-    done < "$functions_file"
+        if [[ -n "$icon" ]]; then
+            printf "%s\0icon\x1f%s\x1finfo\x1f%s\n" "$name" "$icon" "$info"
+        else
+            printf "%s\0info\x1f%s\n" "$name" "$info"
+        fi
+    done < "$actions_file"
 fi
