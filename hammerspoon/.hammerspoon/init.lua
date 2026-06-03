94ed529c67e93bc0ad3cb7f2aa91e83527b85532
Eli Sher <elijsher@gmail.com>
2026-06-03 06:11:50 -0600

    hammerspoon: support both Logitech and Compx mouse receivers for LinearMouse; remove WiFi watcher

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,5 +1,7 @@
 local function usbDeviceCallback(data)
-    if data["productName"] == "USB Receiver" and data["vendorID"] == 1133 then
+    local isLogitech = data["productName"] == "USB Receiver" and data["vendorID"] == 1133
+    local isCompx = data["productName"] == "2.4G Receiver" and data["vendorID"] == 9639
+    if isLogitech or isCompx then
         if data["eventType"] == "added" then
             hs.application.launchOrFocus("LinearMouse")
         elseif data["eventType"] == "removed" then
@@ -11,10 +13,3 @@ end
 
 local usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
 usbWatcher:start()
-
-local wifiWatcher = hs.wifi.watcher.new(function()
-    if hs.wifi.currentNetwork() == "AOL Dialup" then
-        hs.execute("/opt/homebrew/bin/tailscale up", true)
-    end
-end)
-wifiWatcher:start()
