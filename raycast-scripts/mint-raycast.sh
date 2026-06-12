1916ea9ff1d701385f3d04ee7fb8bb86ec09b06e
Eli Sher <elijsher@gmail.com>
2026-06-12 10:30:57 -0600

    remove the mint raycast script - raycast supports this operation natively

--- raycast-scripts/mint-raycast.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# @raycast.schemaVersion 1
-# @raycast.title Open Ghostty window at ~/dev/golang/mintmedia
-# @raycast.mode compact
-
-open -na Ghostty.app --args -e fish -c 'cd ~/dev/golang/mintmedia; exec fish'
