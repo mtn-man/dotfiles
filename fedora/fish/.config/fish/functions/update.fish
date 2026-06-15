19804c4e28c62c7b239cd58da8e4c919b307bbe5
Eli Sher <elijsher@gmail.com>
2026-06-15 05:45:17 -0600

    fedora: update.fish now simply calls the bootstrap script directly

--- fedora/fish/.config/fish/functions/update.fish
+++ fedora/fish/.config/fish/functions/update.fish
@@ -1,7 +1,3 @@
-function update --description 'Update system packages via dnf and flatpak'
-    sudo dnf upgrade --refresh; or return 1
-    flatpak update -y; or return 1
-    flatpak uninstall --unused -y
-
-    echo (set_color normal --bold)"update complete"(set_color normal)
+function update --description 'Update and maintain system via bootstrap script'
+    bash ~/.dotfiles/fedora/fedora-bootstrap.sh
 end
