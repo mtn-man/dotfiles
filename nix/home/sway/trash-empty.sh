739cea7ca69dc531fa885e921ba1d2884eb88da3
Eli Sher <elijsher@gmail.com>
2026-06-25 11:23:07 -0600

    nix: remove nix configuration entirely (not my preference)

--- nix/home/sway/trash-empty.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if pgrep -x rofi > /dev/null; then
-    pkill -x rofi
-    exit
-fi
-choice=$(printf 'Yes\nNo' | rofi -dmenu -p 'Empty Trash?')
-[ "$choice" = 'Yes' ] && trash-empty -f
