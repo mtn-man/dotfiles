2ffc0719d9d17db8aa4f3fb2d7ea01c835c05b91
Eli Sher <elijsher@gmail.com>
2026-07-23 09:42:17 -0600

    server/fish: switch to vim, simplify upgrade abbr

--- server/fish/config.fish
+++ server/fish/config.fish
@@ -10,13 +10,13 @@ if status is-interactive
     set -gx SYSTEMD_PAGER cat
     set -gx EDITOR vim
     #abbrs
-    abbr -s --global update 'sudo dnf upgrade --refresh -y'
+    abbr -a --global u 'sudo dnf upgrade --refresh -y'
     abbr -a --global dr 'doctor'
     abbr -a --global mm 'mintmedia'
     abbr -a --global c 'bat'
     abbr -a --global ts 'tailscale status'
-    abbr -a --global m 'micro'
+    abbr -a --global m 'vim -y'
     abbr -a --global ff 'fastfetch'
     abbr -a --global src 'source $__fish_config_dir/config.fish'
-    abbr -a --global u 'update'
+    abbr -a --global v 'vim'
 end
